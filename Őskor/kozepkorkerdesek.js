4778154d3a0e595d230479bc79d44a1a7b110b9b
Elodman <70641465+Elodman@users.noreply.github.com>
2021-04-03 20:01:59 +0200

    Add files via upload

---
+++ Őskor/kozepkorkerdesek.js
@@ -0,0 +1,48 @@
+feladatok = [
+    {
+        tipus: 1,
+        kerdes: "Mi a cölibátus?",
+        valaszok: ["Az első ún. kolduló rend", "A pápát megválasztó egyházi méltóságok", "A szerzetesek által kézzel másolt mű", "papi nőtlenség"],
+        jovalasz: "papi nőtlenség"
+    },
+    {
+        tipus: 1,
+        kerdes: "Egyszerű, védelmet nyújtó, a mozgást nem nehezítő ruhadarab.",
+        valaszok: ["Láncing", "Bőrpáncél", "Puha vért", "Kemény vért"],
+        jovalasz: "Bőrpáncél"
+    },
+    {
+        tipus: 1,
+        kerdes: "Álmos fia, ő vezette a honfoglaló magyarokat",
+        valaszok: ["Árpád", "Botond", "Lehel", "Szvatopluk"],
+        jovalasz: "Árpád"
+    },
+    {
+        tipus: 1,
+        kerdes: "A tatárjárás Európa felé induló támadások vezére.",
+        valaszok: ["Dzsingisz kán", "Batu kán", "Ögödej nagykán ", "Güjük nagykán"],
+        jovalasz: "Batu kán"
+    },
+    {
+        tipus: 1,
+        kerdes: "Ez a király vezette be az aranybullát",
+        valaszok: ["III.Béla", "Szent László", "II.András", "IV.Béla"],
+        jovalasz: "II.András"
+    },
+    {
+        tipus: 1,
+        kerdes: "Mi a jobbágy?",
+        valaszok: ["Udvari bolond", "Lovag", "Pap", "Paraszt"],
+        jovalasz: "Paraszt"
+    },
+    {
+        tipus: 2,
+        kerdes: "Az aranybulla kiadásának éve",
+        valasz: 1222
+    },
+    {
+        tipus: 2,
+        kerdes: "Mikor kezdődött a középkor?",
+        valasz: 476
+    }
+]
