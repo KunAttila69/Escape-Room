d6def2bf74445c0274e55e513aefc28f5efdecfe
KunAttila69 <71005372+KunAttila69@users.noreply.github.com>
2021-04-05 14:17:32 +0200

    Add files via upload

---
+++ Ókor/okorkerdesek.js
@@ -0,0 +1,48 @@
+feladatok = [
+    {
+        tipus: 1,
+        kerdes: "Melyik a legnagyobb piramis?",
+        valaszok: ["Chichén Itzá", "Louvre", "A Gízai nagy piramis", "Maoling piramis"],
+        jovalasz: "A Gízai nagy piramis"
+    },
+    {
+        tipus: 1,
+        kerdes: "Hogy hívták az egyiptomi uralkodót?",
+        valaszok: ["Fáraó", "Király", "Cár", "Szultán"],
+        jovalasz: "Fáraó"
+    },
+    {
+        tipus: 1,
+        kerdes: "Hol alakult ki az első fejlett civilizáció az ókorban?",
+        valaszok: ["Magyarországon", "Mezopotámiában", "Egyiptomban", "Indiában"],
+        jovalasz: "Mezopotámiában"
+    },
+    {
+        tipus: 1,
+        kerdes: "A monda szerint kik alapították Rómát?",
+        valaszok: ["Caesar és Brutus", "Frédi és Béni", "Romulus és Rémus", "Rómeó és Júlia"],
+        jovalasz: "Romulus és Rémus"
+    },
+    {
+        tipus: 1,
+        kerdes: "Melyik volt az ókor legnagyobb kiterjedésű birodalma?",
+        valaszok: ["Perzsa Birodalom", "Római Birodalom", "Egyiptomi Birodalom", "Mongol Birodalom"],
+        jovalasz: "Perzsa Birodalom"
+    },
+    {
+        tipus: 1,
+        kerdes: "Hogy hívják a görög mitológia fő istenét?",
+        valaszok: ["Kronosz", "Jupiter", "Heraklész", "Zeusz"],
+        jovalasz: "Zeusz"
+    },
+    {
+        tipus: 2,
+        kerdes: "Egyiptomi Birodalom egyesítése (Kr.e.):",
+        valasz: 3000
+    },
+    {
+        tipus: 2,
+        kerdes: "Hány Gízai piramis van?",
+        valasz: "3"
+    }
+]
