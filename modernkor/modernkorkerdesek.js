11da1f1c6d6b4e57a92046e3f62d7244a2321ffd
KunAttila69 <71005372+KunAttila69@users.noreply.github.com>
2021-04-09 09:28:04 +0200

    Add files via upload

---
+++ modernkor/modernkorkerdesek.js
@@ -0,0 +1,48 @@
+feladatok = [
+    {
+        tipus: 1,
+        kerdes: "A második világháború idjén ki volt a szovjetunió minisztertanácsának elnöke?",
+        valaszok: ["Joszif Visszarionovics Sztálin", "Adolf Hitler", "Benito Mussolini", "Nyikita Szergejevics Hruscsov"],
+        jovalasz: "Joszif Visszarionovics Sztálin"
+    },
+    {
+        tipus: 1,
+        kerdes: "Melyik országban élnek a legtöbben?",
+        valaszok: ["India", "Kína", "USA", "Oroszország"],
+        jovalasz: "Kína"
+    },
+    {
+        tipus: 1,
+        kerdes: 'Mi Lengyelország fővárosa?',
+        valaszok: ["Prága", "Stockholm", "Varsó", "Tirane"],
+        jovalasz: "Varsó"
+    },
+    {
+        tipus: 1,
+        kerdes: "Melyik a világ legmagasabb hegycsúcsa?",
+        valaszok: ["Kékestető", "Mont Blanc", "Csomolungma", "Kengamine Peak"],
+        jovalasz: "Csomolungma"
+    },
+    {
+        tipus: 1,
+        kerdes: "Melyik a Földön található legmélyebb árok? ",
+        valaszok: ["Bermuda-háromszög", "Kermadec-árok", "Philippine-árok", "Mariana-árok"],
+        jovalasz: "Mariana-árok"
+    },
+    {
+        tipus: 1,
+        kerdes: "Ki volt a Rubik-kocka feltalálója?",
+        valaszok: ["Rubik Ernő", "Julius Caesar", "Bill Gates", "Neumann János"],
+        jovalasz: "Rubik Ernő"
+    },
+    {
+        tipus: 2,
+        kerdes: "Melyik évben tört ki az első vh?",
+        valasz: "1914"
+    },
+    {
+        tipus: 2,
+        kerdes: "Hány korszaka van az őskornak?",
+        valasz: "3"
+    }
+]
