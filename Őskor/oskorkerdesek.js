63cc928e0d89cffb02bc58df3b9289e4958b792a
Elodman <70641465+Elodman@users.noreply.github.com>
2021-04-09 11:44:13 +0200

    Add files via upload

---
+++ Őskor/oskorkerdesek.js
@@ -0,0 +1,48 @@
+feladatok = [
+    {
+        tipus: 1,
+        kerdes: "Mikortól számítjuk az őskort?",
+        valaszok: ["2000 évvel ezelőtt", "2-3 száz évvel ezelőtt", "6-4 millió évvel ezelőtt", "5-6 ezer évvel ezelőtt"],
+        jovalasz: "6-4 millió évvel ezelőtt"
+    },
+    {
+        tipus: 1,
+        kerdes: "Meddig számítjuk az őskort?",
+        valaszok: ["Krisztus születése", "i.e. 4500", "i.u. 476", "i.e 3000"],
+        jovalasz: "i.e. 4500"
+    },
+    {
+        tipus: 1,
+        kerdes: 'Kit nevezünk az ugynevezett "Előembernek"?',
+        valaszok: ["Miklós", "Samu", "János", "Leonidasz"],
+        jovalasz: "Samu"
+    },
+    {
+        tipus: 1,
+        kerdes: "Mivel vadásztak az őskorban? ",
+        valaszok: ["kézzel", "önöttvas fegyverekkel", "bronzfegyverek", "falándzsával"],
+        jovalasz: "falándzsával"
+    },
+    {
+        tipus: 1,
+        kerdes: "Ki találta meg Samut?",
+        valaszok: ["Petőfi Sándor", "Árpád fejedelem", "Vértes László", "Arany János"],
+        jovalasz: "Vértes László"
+    },
+    {
+        tipus: 1,
+        kerdes: "Mi az őskor utolsó korszaka?",
+        valaszok: ["Újkőkor", "Őskőkor", "Ószövetség", "Reneszánsz"],
+        jovalasz: "Újkőkor"
+    },
+    {
+        tipus: 2,
+        kerdes: "Melyik évben találták meg Samut?",
+        valasz: "1965"
+    },
+    {
+        tipus: 2,
+        kerdes: "Hány korszaka van az őskornak?",
+        valasz: "3"
+    }
+]
